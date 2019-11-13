0b9ed32d551d99e0cec727a244570842d277dc39
andgmartins <and.gmartins@gmail.com>
2019-11-13 16:37:33 -0300

    First Commit Adjust

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = express.Router();
 
-// Define some routes to project
+// Define your routes into project
 routes.get('/', (req,res) => {
     res.send('Start Page by Routes');
 })
